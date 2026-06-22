e7206490204ca76221f16c0c35aa22093201e1cd
kxcd <xkcd@optusnet.com.au>
2026-06-22 09:26:06 +0000

    Fix HTML validation issue

--- index.php
+++ index.php
@@ -315,7 +315,7 @@ foreach ($collateralvalue as $type => $stuff) {
 			<?php echo $UItext["today"]; ?>
 			<span class="info">ℹ️</span>
 		</div>
-		<img src="images/Dash-yield.png" class="tree">
+		<img src="images/Dash-yield.png" alt="Dash money tree" class="tree">
 	</div>
 
 	<!-- 1 Masternode ============ -->
